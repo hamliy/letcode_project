bfd4edf49a80a8ba109d88e6d509855c7a2d4010
lihan_li <lihan_li@kingdee.com>
2020-09-14 16:18:02 +0800

    init

---
+++ app/1_two_sum.py
@@ -0,0 +1,43 @@
+# encoding: utf-8
+"""
+@author: han.li
+@file  : 1_two_sum.py
+@time  : 2020/9/8 下午1:46
+@dec   :
+
+1. 两数之和
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+"""
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        t_map = {}
+        for i, value in enumerate(nums):
+            if target - value in t_map:
+                return [t_map[target-value], i]
+            else:
+                t_map[value] = i
+        return []
+
+
+def test():
+    nums = [2, 7, 11, 15]
+    target = 9
+    print(Solution().twoSum(nums, target))
+    assert(Solution().twoSum(nums, target) == [0, 1])
+
+
+if __name__ == '__main__':
+    test()
+
---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+"""
+@author: han.li
+@file  : __init__.py.py
+@time  : 2020/9/8 下午1:45
+@dec   : 
+"""
+
